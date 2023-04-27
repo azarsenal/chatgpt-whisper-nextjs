c7e3a483beefb01fb03907511a38cec843d95976
Simphoukham <44037594+azarsenal@users.noreply.github.com>
2023-04-27 16:31:48 -0700

    Southin

--- pages/api/whisper.ts
+++ pages/api/whisper.ts
@@ -1,6 +1,9 @@
-const FormData = require('form-data');
+
+import axios from 'axios';
+import FormData from 'form-data';
 import { withFileUpload } from 'next-multiparty';
 import { createReadStream } from 'fs';
+
 
 export const config = {
   api: {
@@ -9,34 +12,51 @@ export const config = {
 };
 
 export default withFileUpload(async (req, res) => {
-  const file = req.file;
-  if (!file) {
-    res.status(400).send('No file uploaded');
-    return;
-  }
+  console.log('Whisper.ts start -----');
 
-  // Create form data
-  const formData = new FormData();
-  formData.append('file', createReadStream(file.filepath), 'audio.wav');
-  formData.append('model', 'whisper-1');
-  const response = await fetch(
-    'https://api.openai.com/v1/audio/transcriptions',
-    {
-      method: 'POST',
-      headers: {
-        ...formData.getHeaders(),
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
-      body: formData,
+  try {
+    // Check if the file is attached to the request
+
+    /*     if (!req.file || !req.file.buffer) {
+      res.status(400).send('No file provided.');
+      return;
+    } */
+
+    const file = req.file;
+    //console.log('------- req.file', req.file);
+    //console.log('------- req.body', req.body);
+    console.log('------- req.file.filepath', req.file?.filepath);
+    if (!file) {
+      res.status(400).send('No file uploaded');
+      return;
     }
-  );
 
-  const { text, error } = await response.json();
-  if (response.ok) {
-    res.status(200).json({ text: text });
-  } else {
-    console.log('OPEN AI ERROR:');
-    console.log(error.message);
-    res.status(400).send(new Error());
+    // Create a FormData instance and append the MP3 file
+    const formData = new FormData();
+    formData.append('file', createReadStream(file.filepath), 'audio.wav');
+    formData.append('model', 'whisper-1');
+
+    // Set up the necessary API headers
+    const headers = {
+      'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    };
+
+    console.log('formData', formData);
+
+    // Make a POST request to the Whisper API
+    const response = await axios.post(
+      'https://api.openai.com/v1/audio/transcriptions',
+      formData,
+      { headers }
+    );
+
+    console.log('response', response.data);
+
+    // Send the response from the API to the client
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(`Error sending MP3 to Whisper API: ${error}`);
+    res.status(500).send('Error sending MP3 to Whisper API.');
   }
 });
